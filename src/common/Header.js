22f18053f7917c001cf426c026d5c9117a0ac1b3
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-05-30 15:28:44 -0400

    updating to connect to server

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Search from "../home/Search";
+import React from 'react';
+import Search from '../home/Search';
 
 function Header ({ constellations, setConstellations }) {
   return (
-    <div className="jumbotron jumbotron-fluid bg-info text-white rounded-0 mb-0">
-      <div className="container">
-        <h1 className="display-4">Explore Constellations</h1>
-        <p className="lead">Explore different constellations in the sky!</p>
+    <div className='jumbotron jumbotron-fluid bg-info text-white rounded-0 mb-0'>
+      <div className='container'>
+        <h1 className='display-4'>I Love Constellations</h1>
+        <p className='lead'>Explore different constellations in the sky!</p>
         <hr />
         <Search
           constellations={constellations}
@@ -15,6 +15,6 @@ function Header ({ constellations, setConstellations }) {
       </div>
     </div>
   );
-};
+}
 
 export default Header;
